4c5e1e2a8fda92f15d504d21c887281ccc5c27c7
Wolity <astapovsasa6@gmail.com>
2025-05-09 16:03:16 +0200

    search

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+import search from "../assets/search.svg";
+import sort from "../assets/sort.svg";
+</script>
+
+<template>
+  <header>
+    <h1>Поиск</h1>
+    <label>
+      <img :src="search" />
+      <input type="text" placeholder="Введие имя, тег, почту..." />
+      <img :src="sort" />
+    </label>
+  </header>
+</template>
+<style scoped lang="scss">
+</style>
